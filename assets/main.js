dbc56b20af2bad84575f5cfac249718ca104b918
Shekhar <chandradaiict@gmail.com>
2017-01-20 13:33:59 +0530

    changes

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,14 @@
 $(function() {
 
   // your code will go here
+  jQuery.ajax({
     url :'https://www.codeschool.com/users/2760097.json',
     dataType:'jsonp',
     success : function({
-
+      for (badge of response.courses.completed) {
+        $("#badegs").appendTo("<div class = course>"+badge+"</div>");
+      }
     })
-  })
+  });
 
 });
